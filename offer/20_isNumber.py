bf7a5cad0728bb2f2a584407758bbe44f31acddc
bpicnbnk <goomarglelly@gmail.com>
2021-04-02 11:54:42 +0800

    revise

--- offer/20_isNumber.py
+++ offer/20_isNumber.py
@@ -43,11 +43,6 @@ class Solution:
         return p in (2, 3, 7, 8)
 
 
-作者：jyd
-链接：https: // leetcode-cn.com/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof/solution/mian-shi-ti-20-biao-shi-shu-zhi-de-zi-fu-chuan-y-2/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-
 s = Solution()
 nums = '1.23e'
 # nums = "1 "
